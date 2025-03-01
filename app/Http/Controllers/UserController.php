98d74a1cca16f5a8a9233fa53df9a59928068b05
rishizurita <zuritarishi@gmail.com>
2025-03-01 23:17:44 +0800

    Configure laravel lumen

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,9 +1,10 @@
 <?php
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Response;
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
 
 class UserController extends Controller
 {
@@ -16,16 +17,15 @@ class UserController extends Controller
     {
         $users = User::all();
         return response()->json(['data' => $users], 200);
-
+    
         // eloquent style
         // $users = user::all();
         
         // sql string as parameter
-        //$users = DB::connection('mysql')
-        //->select("Select * from zuritasialarmen where userid <=3");
+    $users = DB::connection('mysql') ->select("Select * from table_user where userid <=3");
         // return response()->json($users, 200);
         // return response()->json(['data' => $users, 'site' => 1], 200);
-        //retun response()->json(['data' => $users],200);
+    return response()->json(['data' => $users],200);
         // return $this -> successResponse($users);
     }
 
