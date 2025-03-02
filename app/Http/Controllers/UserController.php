46d2ab31bc9d6635b688676e94a62101a2314770
Rishi Zurita <zuritarishi@gmail.com>
2025-03-02 22:45:07 +0800

    Updated files with add and post

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use DB;
 
 
 class UserController extends Controller
 {
+    use ApiResponser;
     private $request;
     public function __construct(Request $request)
     {
@@ -15,18 +18,39 @@ class UserController extends Controller
     }
     public function getUsers()
     {
-        $users = User::all();
-        return response()->json(['data' => $users], 200);
+        //$users = User::all();
+        //return response()->json(['data' => $users], 200);
     
         // eloquent style
         // $users = user::all();
+        // return response()->json($users, 200);
         
         // sql string as parameter
-    $users = DB::connection('mysql') ->select("Select * from table_user where userid <=3");
-        // return response()->json($users, 200);
+    $users = DB::connection('mysql')
+    ->select("Select * from table_user");
+
+    return response()->json($users, 200);
         // return response()->json(['data' => $users, 'site' => 1], 200);
-    return response()->json(['data' => $users],200);
+        //return response()->json(['data' => $users],200);
         // return $this -> successResponse($users);
     }
 
+    public function index()
+    {
+        $users =User::all();
+        return $this->successResponse($users);
+    }
+
+    public function add(Request $request){
+        $rules = [
+            'username' => 'required|max:20',
+            'password' => 'required|max:20',
+            'gender' => 'required|in:Male,Female',
+        ];
+
+        $this->validate($request, $rules);
+        $user = User::create($request->all());
+        return $this->successResponse($user,Response::HTTP_CREATED);
+    }
+    
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+    //name of table
     protected $table = 'table_user';
     // column sa table
     protected $fillable = [
         'username',
-        'password'
+        'password',
+        'gender'
     ];
+    public $timestamps = false;
+
+    // Explicitly disable timestamp fields
+    const CREATED_AT = null;
+    const UPDATED_AT = null;
+    protected $primaryKey = 'userid';
+
 }
