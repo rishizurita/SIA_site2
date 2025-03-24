06905dee4758e55505debc5737bc919cc1facdb8
Rishi Zurita <zuritarishi@gmail.com>
2025-03-24 16:08:29 +0800

    changed lines and add spacing for better reading

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -1,13 +1,15 @@
 <?php
 namespace App\Traits;
+
 use Illuminate\Http\Response;
+
 trait ApiResponser
 {
     /**
      * Build success response
      * @param string|array $data
      * @param int $code
-     * @return Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\JsonResponse
      */
     public function successResponse($data, $code = Response::HTTP_OK)
     {
@@ -17,7 +19,7 @@ trait ApiResponser
      * Build error responses
      * @param string|array $message
      * @param int $code
-     * @return Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\JsonResponse
      */
     public function errorResponse($message, $code)
     {
