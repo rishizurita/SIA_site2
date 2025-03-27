33f389272d9e107a49b92bd24906417a41a44e34
rishizurita <zuritarishi@gmail.com>
2025-03-28 00:50:32 +0800

    database for userjob site2

---
+++ app/Models/UserJob.php
@@ -0,0 +1,23 @@
+<?php
+
+
+// change namespace to App\models if you put your model inside models
+namespace App\Models;
+
+// library to create Model under lumen
+use Illuminate\Database\Eloquent\Model;
+
+class USerJob extends Model{
+
+    // name of table
+    protected $table = "tbluserjob";
+    //column of table
+    protected $fillable = ['jobid','jobname',
+    ];
+
+    // The code will not require the field create_at_and update_at in lumen
+    public $timestamps = false;
+
+    // The code will customized your own primary key field name, default in lumen is id.
+    protected $primaryKey = 'jobid';
+}
